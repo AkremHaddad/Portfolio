b62f97a4fefe81e0c79a71afb6630094aed474c2
Akrem Haddad <akremhaddad125@gmail.com>
2026-07-25 22:17:14 +0100

    changed photo again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
           </div>
           <div className="relative aspect-[4/5] w-full overflow-hidden rounded-[18px]">
             <Image
-              src="/images/me1.png"
+              src="/images/me2.png"
               alt={site.name}
               fill
               priority
